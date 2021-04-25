00147de8f03e34ccf84e7b04acb1cf1b54a9dc71
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-04-25 17:01:37 +0900

    added character movement and screen transition

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    [SerializeField] private float migihaji, hidarihaji;
+    bool shouldFollow = true;
+
+    private void LateUpdate()
+    {
+        shouldFollow = (target.transform.position.x > migihaji || target.transform.position.x < hidarihaji) ? false : true;
+        if (shouldFollow)
+        {
+            followCamera();
+        }
+    }
+
+    private void followCamera()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
