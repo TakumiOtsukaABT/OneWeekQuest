c95da617076c0a92a233d59a854494d98d883a2e
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-08-23 17:05:01 +0900

    add menucanvas

--- Assets/Temporary/UIMenu/MenuCanvas.cs
+++ Assets/Temporary/UIMenu/MenuCanvas.cs
@@ -6,11 +6,23 @@ using UnityEngine.UI;
 public class MenuCanvas : MonoBehaviour
 {
     Stack<GameObject> menuStack = new Stack<GameObject>() { };
+    [SerializeField] GameObject firstButton;
 
-    public void stackWindow(GameObject window)
+    public void Start()
+    {
+        pushWindow(firstButton);
+    }
+
+    public void pushWindow(GameObject window)
     {
         window.SetActive(true);
         menuStack.Push(window);
+    }
+
+    public void popWindow()
+    {
+        var window = menuStack.Pop();
+        window.SetActive(false);
     }
 
 }
