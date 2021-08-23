325ae032e17cabd893382e8ad10a283fd85edd8f
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-08-23 16:55:01 +0900

    set active menu canvas

---
+++ Assets/Temporary/UIMenu/MenuCanvas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuCanvas : MonoBehaviour
+{
+    Stack<GameObject> menuStack = new Stack<GameObject>() { };
+
+    public void stackWindow(GameObject window)
+    {
+        window.SetActive(true);
+        menuStack.Push(window);
+    }
+
+}
