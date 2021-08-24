14c19b300f57ae21b8195d7cba015f332525d8c6
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-08-24 17:30:22 +0900

    add second panels

---
+++ Assets/UIMenu/Script/ButtonsInsideMenu/MenuButtonTemplate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonTemplate : MonoBehaviour
+{
+    [SerializeField] GameObject secondPanel;
+    [SerializeField] MenuCanvas menuCanvas;
+    virtual public void onClick()
+    {
+        menuCanvas.pushWindow(secondPanel);
+    }
+}
