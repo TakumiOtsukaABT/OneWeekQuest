b2b355c7df4bb4e0813e2d313386b1b848b83a45
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-08-25 16:01:11 +0900

    finish status menu

---
+++ Assets/PlayerStats/BaseStatus.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptable")]
+public class BaseStatus : ScriptableObject
+{
+    [SerializeField] private int HP;
+    [SerializeField] private int MP;
+    [SerializeField] private int Attack;
+    [SerializeField] private int Defence;
+    [SerializeField] private int Speed;
+    [SerializeField] private int HitRate;
+    [SerializeField] private int Regen;
+}
