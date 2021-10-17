36506e315430e4f0c7cf0c5406d857f49945a97a
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-10-17 23:17:03 +0900

    handler

--- Assets/BaseTask/TaskHandler.cs
+++ Assets/BaseTask/TaskHandler.cs
@@ -9,12 +9,25 @@ public class TaskHandler : MonoBehaviour
 
     private void passTask()
     {
+        TaskPanel taskPanel = menuCanvas.GetComponent<TaskPanel>();
+        taskPanel._BaseTasks = this.baseTasks;
+    }
 
+    public void tickTask(string key)
+    {
+        foreach (var i in baseTasks)
+        {
+            i.tickCondition(key);
+        }
+        passTask();
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (var i in baseTasks)
+        {
+            i.initDictionary();
+        }
     }
 
     // Update is called once per frame
