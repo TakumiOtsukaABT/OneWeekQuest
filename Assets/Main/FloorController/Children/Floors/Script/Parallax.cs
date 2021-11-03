ebffd002edc9a32c40cf55899900611642378efe
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-11-03 19:17:39 +0900

    capsulate

--- Assets/Main/FloorController/Children/Floors/Script/Parallax.cs
+++ Assets/Main/FloorController/Children/Floors/Script/Parallax.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
     private float length, startpos;
-    public GameObject cam;
+    [SerializeField,ReadOnly] private GameObject grandParent;
+    [SerializeField,ReadOnly] private GameObject cam_0;
     public float parallaxEffect;
 
     // Start is called before the first frame update
     void Start()
     {
+        grandParent = gameObject.transform.parent.parent.parent.gameObject;
+        cam_0 = grandParent.GetComponent<Outlet>().gameObjects[0];
         startpos = transform.localPosition.x;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float dist = (cam.transform.position.x * parallaxEffect);
+        float dist = (cam_0.transform.position.x * parallaxEffect);
         transform.localPosition = new Vector3(startpos + dist, transform.localPosition.y, transform.localPosition.z);
     }
 }
