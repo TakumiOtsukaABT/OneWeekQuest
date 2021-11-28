053d8c60a5974bf6808760bd29dca0e9bda1c23a
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-11-28 16:45:51 +0900

    add growth board

--- Assets/Main/UIMenu/Script/Board/GrowthBoard.cs
+++ Assets/Main/UIMenu/Script/Board/GrowthBoard.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class GrowthBoard : Board
 {
-    //public override void updateText(string newText)
-    //{
-    //    base.updateText(newText);
-    //    selectAction();
-    //}
+    [SerializeField] private GameObject CharacterHouse;
 
-    //protected virtual void selectAction()
-    //{
-    //}
+    protected FadeoutCharacterController fadeoutCharacterController;
+    public void onButtonPush()
+    {
+        StartCoroutine(waitGrowthAnimation());
+    }
+
+    private IEnumerator waitGrowthAnimation()
+    {
+        Outlet outlet = CharacterHouse.GetComponent<Outlet>();
+        BlackoutController blackout = outlet.gameObjects[1].GetComponent<BlackoutController>();
+        fadeoutCharacterController = outlet.gameObjects[2].GetComponent<FadeoutCharacterController>();
+        blackout.ActivateFadeout();
+        childActivation();
+        childTakeEffect();
+        yield return new WaitForSeconds(3);
+        blackout.DeactivateFadeoutWithDelay(0);
+        childDeactivation();
+    }
+
+    virtual public void childActivation()
+    {
+
+    }
+
+    virtual public void childDeactivation()
+    {
+
+    }
+
+    virtual public void childTakeEffect()
+    {
+
+    }
 }
