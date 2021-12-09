f8994f14f53c99fc80250375113eada846afd5a0
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-12-10 00:33:24 +0900

    aaaa

---
+++ Assets/Main/DialogueCanvasForBattle/Script/DialogueCanvasCommand.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamekit2D;
+namespace Gamekit2D
+{
+    public class DialogueCanvasCommand : DialogueCanvasController
+    {
+        private void Start()
+        {
+            
+        }
+    }
+}
