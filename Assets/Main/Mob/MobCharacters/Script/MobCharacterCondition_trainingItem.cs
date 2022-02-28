7806cacab72bee1303d2275114619d56e94ec468
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2022-03-01 01:47:38 +0900

    inin

--- Assets/Main/Mob/MobCharacters/Script/MobCharacterCondition_trainingItem.cs
+++ Assets/Main/Mob/MobCharacters/Script/MobCharacterCondition_trainingItem.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class MobCharacterCondition_trainingItem : MobCharacterController
 {
+    public string[] baaberu;
+    public string[] balanceball;
+    public string[] jitensha;
+    public string[] after;
+    public string[] default_;
+    public int[] baaberuId;
+    public int[] balanceballId;
+    public int[] jitenshaId;
+
     protected override void setDialogueAndItemWithCondition()
     {
         base.setDialogueAndItemWithCondition();
