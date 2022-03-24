16c2d0b9ff20dc0b33185fbeafa9aaf8bac12663
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2022-03-25 01:55:05 +0900

    done coffee

--- Assets/Main/Mob/MobCharacters/Script/MobCharacterConditionCoffee.cs
+++ Assets/Main/Mob/MobCharacters/Script/MobCharacterConditionCoffee.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MobCharacterConditionCoffee : MonoBehaviour
+public class MobCharacterConditionCoffee : MobCharacterController
 {
-    // Start is called before the first frame update
-    void Start()
+    public string[] coffee;
+    public string[] after;
+    public int[] coffeeId;
+    [SerializeField] private string[] notBuffed;
+    [SerializeField] private int chocolateBuffId;
+    protected override void setDialogueAndItemWithCondition()
     {
-        
-    }
+        PlayerBuff playerBuff = base.playerData_1.GetComponent<PlayerBuff>();
+        PlayerInventory playerInventory = base.playerData_1.GetComponent<PlayerInventory>();
+        if (!playerInventory.hasItem(coffeeId[0]))
+        {
+            if (playerBuff.GetElement(chocolateBuffId) != 99)
+            {
+                base.dialogue = coffee;
+                base.itemId = coffeeId;
+                base.gave = true;
+            }
+            else
+            {
+                base.dialogue = notBuffed;
+            }
+        }
+        else
+        {
+            base.dialogue = after;
+            base.itemId = null;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
