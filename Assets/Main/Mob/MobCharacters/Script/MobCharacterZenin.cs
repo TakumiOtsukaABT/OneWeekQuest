f5c7733461ce934f5680f6a1bddee9cc529f8ee5
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2022-03-25 19:43:17 +0900

    aaaaaa

--- Assets/Main/Mob/MobCharacters/Script/MobCharacterZenin.cs
+++ Assets/Main/Mob/MobCharacters/Script/MobCharacterZenin.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class MobCharacterZenin : MobCharacterController
 {
-    // Start is called before the first frame update
-    void Start()
+    public string[] zeninText;
+    public string[] after;
+    public int[] ZeninId;
+    [SerializeField] private string[] not;
+    [SerializeField] private int[] PromisesId;
+    protected override void setDialogueAndItemWithCondition()
     {
-        
-    }
+        PlayerBuff playerBuff = base.playerData_1.GetComponent<PlayerBuff>();
+        PlayerInventory playerInventory = base.playerData_1.GetComponent<PlayerInventory>();
+        if (!playerInventory.hasItem(ZeninId[0]))
+        {
+            bool hasAllPromises = true;
+            foreach(var i in PromisesId)
+            {
+                if (playerBuff.GetElement(i) == 99) hasAllPromises = false;
+            }
+            if (hasAllPromises)
+            {
+                base.dialogue = zeninText;
+                base.itemId = ZeninId;
+                base.gave = true;
+            }
+            else
+            {
+                base.dialogue = not;
+            }
+        }
+        else
+        {
+            base.dialogue = after;
+            base.itemId = null;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
