36d138d257e5512f07b7fe6e99d4b3c2095460ee
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2022-04-05 02:44:40 +0900

    yea

--- Assets/Main/Battle/CommandButton/ButtonScript/ReviveAction.cs
+++ Assets/Main/Battle/CommandButton/ButtonScript/ReviveAction.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReviveAction : MonoBehaviour
+public class ReviveAction : BaseActionCommand
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, ReadOnly] protected GameDirector gameDirector_3;
+    public GameObject dialogueCanvasCommand;
+    [SerializeField] float editMultiplier;
+    public override void runActionCommand()
     {
-        
+        gameDirector_3 = dialogueCanvasCommand.GetComponent<Outlet>().gameObjects[3].GetComponent<GameDirector>();
+        gameDirector_3.resetState(BattleState.SelectTarget);
+        gameDirector_3.selectingType = SelectingType.Single;
+        Debug.Log("ran until");
+        selectTarget();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void selectTarget()
     {
-        
+        StartCoroutine(chooseTarget());
+    }
+    virtual protected IEnumerator chooseTarget()
+    {
+        Debug.Log(gameDirector_3.getFlagDoneSelecting());
+        yield return new WaitUntil(gameDirector_3.getFlagDoneSelecting);
+        Debug.Log("ran untilwait");
+        Debug.Log(gameDirector_3.getFlagDoneSelecting());
+        int regen = gameDirector_3.getCurrentCharacter().GetComponent<StatusBattle>().playerStatusForReference.Regen_access;
+        gameDirector_3.setHealAndDialogue(Mathf.RoundToInt(regen * editMultiplier));
+        gameDirector_3.resetState(BattleState.Read, battleEffect: base.Effect);
     }
 }
