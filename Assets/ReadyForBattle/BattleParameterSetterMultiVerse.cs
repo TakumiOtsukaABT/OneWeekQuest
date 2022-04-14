3a6cf7f04c090c3982e153aeb5e990b661cc774b
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2022-04-15 00:52:46 +0900

    a little bit of change

--- Assets/ReadyForBattle/BattleParameterSetterMultiVerse.cs
+++ Assets/ReadyForBattle/BattleParameterSetterMultiVerse.cs
@@ -5,26 +5,51 @@ using UnityEngine;
 public class BattleParameterSetterMultiVerse : MonoBehaviour
 {
     [SerializeField, ReadOnly] GameObject playerDataHandler;
-    [SerializeField, ReadOnly] StatusBattle alpacaStatusBattle;
-    [SerializeField, ReadOnly] StatusBattle dogStatusBattle;
-    [SerializeField, ReadOnly] StatusBattle catStatusBattle;
-    [ReadOnly] StatusBattle characterStatusBattle;
+    [SerializeField, ReadOnly] BattleParameterStorer alpacaStatusBattle = new BattleParameterStorer();
+    [SerializeField, ReadOnly] BattleParameterStorer dogStatusBattle = new BattleParameterStorer();
+    [SerializeField, ReadOnly] BattleParameterStorer catStatusBattle = new BattleParameterStorer();
+    [SerializeField, ReadOnly] BattleParameterStorer characterStatusBattle = new BattleParameterStorer();
 
-    bool alpacaActive = false;
-    bool dogActive = false;
-    bool catActive = false;
+    [SerializeField, ReadOnly] bool alpacaActive = false;
+    [SerializeField, ReadOnly] bool dogActive = false;
+    [SerializeField, ReadOnly] bool catActive = false;
 
     // Start is called before the first frame update
     void Start()
     {
         playerDataHandler = GameObject.Find("PlayerDataHandlerMultiVerse");
-        characterStatusBattle.playerStatusForReference = playerDataHandler.GetComponent<PlayerStatus>().playerStatusForReference;
-        characterStatusBattle.name = playerDataHandler.GetComponent<PlayerStatus>().name;
+        //for (int i = 0; i<100;i++)
+        //{
+
+        //        characterStatusBattle.playerStatusForReference = playerDataHandler.GetComponent<PlayerStatus>().playerStatusForReference;
+        //        characterStatusBattle.name = playerDataHandler.GetComponent<PlayerStatus>().name;
+        //}
         //後はここから、インベントリから仲間の有無、バトルコマンドの設定
+
+        PlayerInventory inventory = playerDataHandler.GetComponent<PlayerInventory>();
+        foreach(var i in inventory.IdList)
+        {
+            checkActiveAllies(ref alpacaActive, i, 21);
+            checkActiveAllies(ref dogActive, i, 11);
+            checkActiveAllies(ref catActive, i, 15);
+        }
+    }
+
+    private void checkActiveAllies(ref bool activeFlag, int iteration, int flagID)
+    {
+        if(iteration == flagID)
+        {
+            activeFlag = true;
+        }
     }
 
     private void setStatusBattle(StatusBattle to)
     {
 
     }
+}
+
+public class BattleParameterStorer
+{
+
 }
