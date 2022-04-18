cbab65819a242e516bd44fd7789ee69aedde8bfc
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2022-04-18 22:49:54 +0900

    add battlecommand update func

--- Assets/Main/Battle/WazaContent/WazaContent.cs
+++ Assets/Main/Battle/WazaContent/WazaContent.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WazaContent : MonoBehaviour
 {
+    public GameObject dialogueCanvasCommand;
     public List<GameObject> techniquePrefabs =  new List<GameObject>();
+    private Outlet outlet;
     void OnEnable()
     {
-        //outlet = menuCanvas.GetComponent<Outlet>();
-        //List<int> commands_id_list = outlet.gameObjects[0].GetComponent<CommandList>().IdList;
-        //foreach (int i in commands_id_list)
-        //{
-        //    setPrefab(i);
-        //    GameObject instantiated = Instantiate(prefab, this.transform);
-        //    instantiated.GetComponent<Button>().onClick.AddListener(() => {
-        //        setListener(i);
-        //    });
-        //}
+        outlet = dialogueCanvasCommand.GetComponent<Outlet>();
+        GameDirector gameDirector = outlet.gameObjects[3].GetComponent<GameDirector>();
+        List<int> idList = gameDirector.getCurrentCharacter().GetComponent<StatusBattle>().battleCommandID;
+        foreach(var i in GameObject.FindGameObjectsWithTag("Waza"))
+        {
+            Destroy(i);
+        }
+        foreach(int i in idList)
+        {
+            GameObject instantiated = Instantiate(techniquePrefabs[i], this.transform);
+            instantiated.GetComponent<WazaAction>().dialogueCanvasCommand = dialogueCanvasCommand;
+            instantiated.GetComponent<Button>().onClick.AddListener(() => {
+                dialogueCanvasCommand.GetComponent<DialogueCanvasCommand>().onButtonClick(instantiated);
+            });
+        }
     }
 }
