d47fd33f128878a1203f6a76c5532ab8c609aa18
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2022-05-10 01:35:53 +0900

    ready for town

---
+++ Assets/Main/Ready/OpeningDialogueCanvas.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningDialogueCanvas : DialogueCanvasController
+{
+    protected override void setInputHandleBack()
+    {
+        //inputController_1.setInputHandle<CharacterMovementInputHandle>();
+    }
+
+    protected override void tickTask()
+    {
+        //base.tickTask();
+    }
+}
---
+++ Assets/Main/Ready/OpeningSetter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningSetter : MonoBehaviour
+{
+    public List<string> dialogue = new List<string>();
+    public DialogueCanvasController dialogueCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueCanvas.Dialogue = dialogue.ToArray();
+        dialogueCanvas.ActivateCanvasWithDialogueArray();
+    }
+
+}
