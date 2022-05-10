d368537266419aa9a8f6632a01e05fb8328a4b77
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2022-05-11 01:30:18 +0900

    set up namae

--- Assets/Main/Ready/NameInputField.cs
+++ Assets/Main/Ready/NameInputField.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class NameInputField : MonoBehaviour
 {
+    public string name_character;
+    public Text text;
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += SceneLoaded;
+    }
     public void onClickConfirm()
     {
-
+        name_character = text.text;
         SceneManager.LoadScene("Town");
+    }
+
+    void SceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        GameObject playerDataHandler = GameObject.Find("PlayerDataHandler");
+        GameObject playerDataHandlerMultiVerse = GameObject.Find("PlayerDataHandlerMultiVerse");
+        playerDataHandler.GetComponent<PlayerStatus>().name = name_character;
+        playerDataHandlerMultiVerse.GetComponent<PlayerStatus>().name = name_character;
     }
 }
