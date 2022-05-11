a11baa161d0e19bab2335f678faaf9cb06d6cac1
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2022-05-12 01:25:34 +0900

    aaa

--- Assets/ReadyForBattle/ReadyForBattleDialogueController.cs
+++ Assets/ReadyForBattle/ReadyForBattleDialogueController.cs
@@ -7,10 +7,18 @@ using UnityEngine.SceneManagement;
 public class ReadyForBattleDialogueController : DialogueCanvasController
 {
     public string battleSceneName;
+    public BlackoutController blackout;
     protected override void setInputHandleBack()
     {
         //inputController_1.setInputHandle<CharacterMovementInputHandle>();
         inputController_1.setInputHandle<MenuInputHandle>();
+        StartCoroutine(waitActiveFadeout());
+    }
+
+    IEnumerator waitActiveFadeout()
+    {
+        blackout.ActivateFadeout();
+        yield return new WaitForSeconds(2.0f);
         SceneManager.LoadScene("Battle");
     }
 
