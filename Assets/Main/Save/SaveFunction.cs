07e558583625b6b01e35e24dc5f7f4fec7bd83a4
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2022-06-04 00:56:37 +0900

    Savefunction

--- Assets/Main/Save/SaveFunction.cs
+++ Assets/Main/Save/SaveFunction.cs
@@ -6,49 +6,42 @@ using System.IO;
 public class SaveFunction : MonoBehaviour
 {
     public GameObject playerdataHandler;
-    [SerializeField, ReadOnly] private PlayerStatus status;
-    [SerializeField, ReadOnly] private PlayerBattleCommandList battleCommandList;
-    [SerializeField, ReadOnly] private PlayerInventory playerInventory;
-    [SerializeField, ReadOnly] private PlayerTrainingCommandsList playerTrainingCommandsList;
-    [SerializeField, ReadOnly] private PlayerStudyCommandsList playerStudyCommandsList;
-    [SerializeField, ReadOnly] private PlayerEatCommandsList playerEatCommandsList;
-    [SerializeField, ReadOnly] private PlayerBuff playerBuff;
+    //[SerializeField, ReadOnly] private PlayerStatus status;
+    //[SerializeField, ReadOnly] private PlayerBattleCommandList battleCommandList;
+    //[SerializeField, ReadOnly] private PlayerInventory playerInventory;
+    //[SerializeField, ReadOnly] private PlayerTrainingCommandsList playerTrainingCommandsList;
+    //[SerializeField, ReadOnly] private PlayerStudyCommandsList playerStudyCommandsList;
+    //[SerializeField, ReadOnly] private PlayerEatCommandsList playerEatCommandsList;
+    //[SerializeField, ReadOnly] private PlayerBuff playerBuff;
     // Start is called before the first frame update
     void Start()
     {
-        StreamWriter writer;
-        setEveryComponent();
-        ClassesInPlayerHandler classesInPlayerDataHandler = new ClassesInPlayerHandler();
-        classesInPlayerDataHandler.status = status;
-        //classesInPlayerDataHandler.playerInventory = playerInventory;
-        //classesInPlayerDataHandler.playerTrainingCommandsList = playerTrainingCommandsList;
-        //classesInPlayerDataHandler.playerStudyCommandsList = playerStudyCommandsList;
-        //classesInPlayerDataHandler.playerEatCommandsList = playerEatCommandsList;
-        //classesInPlayerDataHandler.playerBuff = playerBuff;
-        string jsonstr = JsonUtility.ToJson(playerInventory,true);
-        //jsonstr += JsonUtility.ToJson(battleCommandList);
-        //jsonstr += JsonUtility.ToJson(playerInventory);
-        //jsonstr += JsonUtility.ToJson(playerTrainingCommandsList);
-        //jsonstr += JsonUtility.ToJson(playerStudyCommandsList);
-        //jsonstr += JsonUtility.ToJson(playerEatCommandsList);
-        //jsonstr += JsonUtility.ToJson(playerBuff);
+        //StreamWriter writer;
+        ////setEveryComponent();
+        //ClassesInPlayerHandler classesInPlayerDataHandler = new ClassesInPlayerHandler(playerdataHandler);
+        //string jsonstr = JsonUtility.ToJson(classesInPlayerDataHandler,true);
+        //writer = new StreamWriter(Application.dataPath + "/savedata.json", false);
+        //writer.Write(jsonstr);
+        //writer.Flush();
+        //writer.Close();
+        string datastr = "";
+        StreamReader reader;
+        reader = new StreamReader(Application.dataPath + "/savedata.json");
+        datastr = reader.ReadToEnd();
+        reader.Close();
 
-
-        writer = new StreamWriter(Application.dataPath + "/savedata.json", false);
-        writer.Write(jsonstr);
-        writer.Flush();
-        writer.Close();
+        ClassesInPlayerHandler a = JsonUtility.FromJson<ClassesInPlayerHandler>(datastr);
+        Debug.Log(a.playerStudyCommand_IdList[2]);
     }
 
-    private void setEveryComponent()
-    {
-        status = playerdataHandler.GetComponent<PlayerStatus>();
-        battleCommandList = playerdataHandler.GetComponent<PlayerBattleCommandList>();
-        playerInventory = playerdataHandler.GetComponent<PlayerInventory>();
-        playerTrainingCommandsList = playerdataHandler.GetComponent<PlayerTrainingCommandsList>();
-        playerStudyCommandsList = playerdataHandler.GetComponent<PlayerStudyCommandsList>();
-        playerEatCommandsList = playerdataHandler.GetComponent<PlayerEatCommandsList>();
-        playerBuff = playerdataHandler.GetComponent<PlayerBuff>();
-    }
-
+    //private void setEveryComponent()
+    //{
+    //    status = playerdataHandler.GetComponent<PlayerStatus>();
+    //    battleCommandList = playerdataHandler.GetComponent<PlayerBattleCommandList>();
+    //    playerInventory = playerdataHandler.GetComponent<PlayerInventory>();
+    //    playerTrainingCommandsList = playerdataHandler.GetComponent<PlayerTrainingCommandsList>();
+    //    playerStudyCommandsList = playerdataHandler.GetComponent<PlayerStudyCommandsList>();
+    //    playerEatCommandsList = playerdataHandler.GetComponent<PlayerEatCommandsList>();
+    //    playerBuff = playerdataHandler.GetComponent<PlayerBuff>();
+    //}
 }
