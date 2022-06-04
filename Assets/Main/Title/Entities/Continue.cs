4e1eff00a5e47e8bc5ae02ae0be2e3f69fbb0cf3
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2022-06-04 17:50:35 +0900

    unchingpunch

--- Assets/Main/Title/Entities/Continue.cs
+++ Assets/Main/Title/Entities/Continue.cs
@@ -10,6 +10,7 @@ public class Continue : MonoBehaviour
 {
     string datastr = "";
     StreamReader reader;
+    public GameObject playerMultiverse;
     public void Onclick()
     {
         try {
@@ -17,10 +18,24 @@ public class Continue : MonoBehaviour
             datastr = reader.ReadToEnd();
             reader.Close();
             ClassesInPlayerHandler PH = JsonUtility.FromJson<ClassesInPlayerHandler>(datastr);
+            continueDataPass(playerMultiverse, PH);
             SceneManager.LoadScene(PH.Day);
         } catch (FileNotFoundException)
         {
             GameObject.Find("AlertCanvas").GetComponent<AlertScript>().Activate("セーブファイルが見つかりません");
         }
      }
+    private void continueDataPass(GameObject playerMultiverse, ClassesInPlayerHandler classesInPlayer)
+    {
+        playerMultiverse.GetComponent<PlayerStatus>().playerStatusForReference = classesInPlayer.playerStatus_status;
+        playerMultiverse.GetComponent<PlayerBattleCommandList>().IdList = classesInPlayer.playerBattleCommand_IdList;
+        playerMultiverse.GetComponent<PlayerInventory>().IdList = classesInPlayer.playerInventory_IdList;
+        playerMultiverse.GetComponent<PlayerInventory>().countList = classesInPlayer.playerInventory_CountList;
+        playerMultiverse.GetComponent<PlayerTrainingCommandsList>().IdList = classesInPlayer.playerTrainingCommand_List;
+        playerMultiverse.GetComponent<PlayerStudyCommandsList>().IdList = classesInPlayer.playerStudyCommand_IdList;
+        playerMultiverse.GetComponent<PlayerStudyCommandsList>().count = classesInPlayer.playerStudyCommand_CountList;
+        playerMultiverse.GetComponent<PlayerEatCommandsList>().IdList = classesInPlayer.playerEatCommand_IdList;
+        playerMultiverse.GetComponent<PlayerBuff>().buff_list = classesInPlayer.playerBuff_BuffList;
+        playerMultiverse.GetComponent<PlayerStatus>().name = classesInPlayer.playerStatus_name;
+    }
 }
